c2ab270a31e954543bd2f254ca4a6512a3536de3
jia wei <jw397@126.com>
2021-11-29 14:53:42 +0800

    feat: remove lock

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deno run --allow-net --allow-env --allow-write --allow-read --lock lock.json --config tsconfig.json --unstable src/main.ts
+deno run --allow-net --allow-env --allow-write --allow-read --config tsconfig.json --unstable src/main.ts
