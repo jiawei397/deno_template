25abe8855d2104b8bddc60acdfcefc1a866335dc
jw397 <jw397@126.com>
2022-11-17 09:51:03 +0800

    feat: 添加全局异常捕获

--- src/main.ts
+++ src/main.ts
@@ -22,4 +22,13 @@ app.use(app.routes());
 
 const port = Number(Deno.env.get("PORT") || globals.port);
 logger.info(`app start with: http://localhost:${port}`);
+addEventListener("unhandledrejection", (evt) => {
+  evt.preventDefault();
+  logger.error(`unhandledrejection`, evt.reason);
+});
+
+addEventListener("error", (evt) => {
+  evt.preventDefault(); // 这句很重要
+  logger.error(`global error`, evt.error);
+});
 await app.listen({ port });
